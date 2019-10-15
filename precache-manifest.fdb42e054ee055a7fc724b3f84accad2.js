c3a10ca8b749ce0852492e51c69a99f33b1a6842
Glievoyss <glievoy.s@gmail.com>
2019-10-15 17:41:11 +0300

    Updates

---
+++ precache-manifest.fdb42e054ee055a7fc724b3f84accad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dafc339e7f1d6e3aca1e5fd2e0f3ea3",
+    "url": "/goit-react-hw-03-bank-account/./index.html"
+  },
+  {
+    "revision": "4a3a4ce01f83a9909964",
+    "url": "/goit-react-hw-03-bank-account/./static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "0d06911e42bb8806ee1a",
+    "url": "/goit-react-hw-03-bank-account/./static/css/main.d91058d7.chunk.css"
+  },
+  {
+    "revision": "4a3a4ce01f83a9909964",
+    "url": "/goit-react-hw-03-bank-account/./static/js/2.c0e9b367.chunk.js"
+  },
+  {
+    "revision": "0d06911e42bb8806ee1a",
+    "url": "/goit-react-hw-03-bank-account/./static/js/main.10b53bfe.chunk.js"
+  },
+  {
+    "revision": "a495152a9f24abae8b66",
+    "url": "/goit-react-hw-03-bank-account/./static/js/runtime-main.9a4df362.js"
+  }
+]);
